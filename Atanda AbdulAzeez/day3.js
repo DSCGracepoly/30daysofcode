380df95ca3d515da7dec40b3d9cb22ad1aa3a73f
AbdulAzeez Atanda <aatanda.dammy@gmail.com>
2019-01-10 01:40:20 +0100

    write function that reverses every string

--- Atanda AbdulAzeez/day3.js
+++ Atanda AbdulAzeez/day3.js
@@ -1,3 +1,9 @@
 const reverseString = string =>{
-    
+    let reversedString = "";
+    for (let i = string.length; i >= 0; i--){
+       reversedString += string.charAt(i);
+    }
+    return reversedString;
 }
+
+console.log(reverseString("nurses run??"));
