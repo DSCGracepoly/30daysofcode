a8e391ca1feb9549716e5c82afd17b510910d9a4
Oluwatobiloba Olusoga <oluwatobilobaolusoga@gmail.com>
2019-01-23 17:27:42 +0100

    Delete day7.js

--- Oluwatobiloba Olusoga/day7.js
+++
@@ -1,9 +0,0 @@
-function objectToArray(obj) {
-    
-    let twoDArray = Object.entries(obj);
-    
-    console.log(twoDArray);
-};
-
-const obj = { a: 1, b: 2, c: 3, d: 4 };
-window.onload = objectToArray(obj);
