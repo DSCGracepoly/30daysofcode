98849044188fc9fe080e28942c04047150ee558f
Oluwatobiloba Olusoga <oluwatobilobaolusoga@gmail.com>
2019-02-06 17:29:20 +0100

    Update day10.js

--- Oluwatobiloba Olusoga/day10.js
+++ Oluwatobiloba Olusoga/day10.js
@@ -1,43 +1,19 @@
-let ghv;
-let row = 100;
-let col = 3;
-let tabili = document.getElementById('gitHubUsers');
+let binaryString = '';
 
-const fetchFromApi = async () => {
-    const responsev = await fetch('https://api.github.com/search/users?q=location%3Anigeria&per_page=100');
-    ghv = await responsev.json();
-    createTable();
-}
-
-function createTable(){
-    for (var i = 0; i < row; i++){
-        var tr = document.createElement('tr');
-        
-        for (j = 0; j < col; j++){
-            let text;
-            let td = document.createElement('td');
-            if (j == 0){
-                text = document.createTextNode(i + 1);
-                td.appendChild(text);
-            }
-            else if(j == 1){
-                text = document.createTextNode(ghv.items[i].login);
-                td.appendChild(text);
-            }
-            else{
-                let newlink = document.createElement('a');
-                newlink.setAttribute('href', ghv.items[i].html_url);
-                text = document.createTextNode('Check out user on GitHub');
-                newlink.appendChild(text);
-                td.appendChild(newlink);
-            }
-            tr.appendChild(td);
-        }
-        tabili.appendChild(tr);
-      }
-}
-
-fetchFromApi();
-
+function convertToBinary(num){
+  console.log('first ' + binaryString);
+  binaryString = num % 2 + binaryString;
+  if (num > 1){
+    convertToBinary(Math.trunc(num/2));
+  }
+  console.log(binaryString);
   
- 
+}
+
+function resetBinaryString(num){
+  binaryString = '';           
+  convertToBinary(num);
+}
+
+const numToBeConverted = 4;
+window.onload = convertToBinary(numToBeConverted);
