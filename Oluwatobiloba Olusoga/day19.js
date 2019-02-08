3e0db6f26ce6be4b43e11b511904495db8eee5a3
Oluwatobiloba Olusoga <oluwatobilobaolusoga@gmail.com>
2019-02-08 20:27:35 +0100

    Update day19.js

--- Oluwatobiloba Olusoga/day19.js
+++ Oluwatobiloba Olusoga/day19.js
@@ -1,80 +1,8 @@
-function validateString(str){
-    
-    str.trim();
-    noOfCurlyOpeners = (str.match(/{/g)||[]).length;
-    noOfCurlyClosers = (str.match(/}/g)||[]).length;
-    noOfSquareOpeners = str.split('[').length - 1;
-    noOfSquareClosers = str.split(']').length - 1;
-    noOfRoundOpeners = str.split('[').length - 1;
-    noOfRoundClosers = str.split(']').length - 1;
-    
-    let stack = [];
-    
-    str = str.replace(/[^{}()\]\[]/g, '');
-    // console.log(str);
-    if (noOfCurlyOpeners != noOfCurlyClosers  ){
-      return 'false';
-    }
-    if (noOfSquareOpeners != noOfSquareClosers  ){
-      return 'false';
-    }
-    if (noOfRoundOpeners != noOfRoundClosers  ){
-      return 'false';
-    }
-  
-    
-//     if (str[1] == ']' || str[1] == ')' ){
-//         return false;
-//       }
-
-    
-    let closers = {
-      '{': '}',
-      '[': ']',
-      '(': ')',
-    };
-    
-    str.split('').forEach(function(character, i){
-      
-      let position = i + 1;
-      
-      if(!~'({[)}]'.indexOf(character)){
-        
-       
-        return;
-        
-      } else if(~'({['.indexOf(character)){
-        
-        stack.push(character);
-        
-      } else if(!stack.length){
-        
-        return false;
-        
-      } else if(~')}]'.indexOf(character)){
-        
-        let characterToClose = stack.pop();
-        let expectedCloser = closers[characterToClose];
-        
-        if(character !== expectedCloser){
-            return false;
-        }
-      }
-    });
-    
-   
-    if(stack.length){
-        return false;
-    }
-    
-    return true;
-    
-  }
-  // console.log(validateString("{ [ ( ] ) }" ));
- 
-function validateCode(stringToBeValidated){
-  console.log(validateString(stringToBeValidated));
+function getInitials(name){
+    name = name.split(' ');
+    let initials = name[0][0].toUpperCase() + ' ' + name[1][0].toUpperCase();
+    console.log(initials);
 }
 
-const stringToBeValidated = '{ [ ( ] ) }';
-window.onload = console.log(validateString(stringToBeValidated));
+const name = 'Steve Brian';
+window.onload = getInitials(name);
